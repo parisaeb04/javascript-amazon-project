e8b1c0fc1d6e96b6c1aa36368d4e69fb00b282be
parisa.eb <parisa.ebrahimian83@gmail.com>
2024-08-18 21:38:41 +0330

    Delete products from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -15,7 +15,7 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML +=  `
-      <div class="cart-item-container">
+      <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
           Delivery date: Tuesday, June 21
         </div>
@@ -101,6 +101,9 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
 
     removeFromCart(productId);
 
-    
+    const container = 
+      document.querySelector(`.js-cart-item-container-${productId}`);
+
+    container.remove();
   });
 });
